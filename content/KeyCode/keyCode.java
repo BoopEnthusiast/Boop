10bbf940e5d4fde164c4232bcc66e6996945c1c7
Boop <onpotis@gmail.com>
2022-12-03 01:08:21 +1300

    Added another hidden files thingy and removed old one

--- content/KeyCode/keyCode.java
+++ content/KeyCode/keyCode.java
@@ -1,6 +1,7 @@
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.util.ArrayList;
+import java.io.*;
 
 public class keyCode {
     public static void main(String[] args) {
@@ -16,25 +17,62 @@ public class keyCode {
             symbols.add("R");
             symbols.add("Select");
             symbols.add("Start");
-            for(int i=0;i<5;
-            for(String symbol : symbols) {
-                File folder = new File(symbol);
-                File myObj = new File("README.md");
-                if (folder.mkdir()) {
-                    System.out.println("File created: " + folder.getName());
-                } else {
-                    System.out.println("File already exists.");
+            File copy = new File("dictionary.txt");
+            for(int i=0;i<6;i++){
+                for(String symbol : symbols) {
+                    for(String symbol2 : symbols) {
+                        for(String symbol3 : symbols) {
+                            for(String symbol4 : symbols) {
+                                File folder = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol);
+                                File myObj = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+"README.md");
+                                if(i == 1) {
+                                    folder = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol+"\\"+symbol2);
+                                    myObj = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol+"\\"+"README.md");
+                                }else if(i == 2) {
+                                    folder = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol+"\\"+symbol2+"\\"+symbol3);
+                                    myObj = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol+"\\"+symbol2+"\\"+"README.md");
+                                }else if(i == 3) {
+                                    folder = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol+"\\"+symbol2+"\\"+symbol3+"\\"+symbol4);
+                                    myObj = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol+"\\"+symbol2+"\\"+symbol3+"\\"+"README.md");
+                                }else if(i == 4) {
+                                    myObj = new File("D:\\Hugo\\Sites\\Boop\\content\\Labyrinth\\"+symbol+"\\"+symbol2+"\\"+symbol3+"\\"+symbol4+"\\"+"README.md");
+                                }
+                                if (folder.mkdir()) {
+                                    System.out.println("File created: " + folder.getName());
+                                } else {
+                                    System.out.println("File already exists.");
+                                }
+                                if (myObj.createNewFile()) {
+                                    System.out.println("File created: " + myObj.getName());
+                                    copyFileUsingStream(copy, myObj);
+                                }else {
+                                    System.out.println("File already exists.");
+                                }
+                            }
+                        }
+                    }
                 }
-                
             }
-            if (myObj.createNewFile()) {
-                    System.out.println("File created: " + myObj.getName());
-                } else {
-                    System.out.println("File already exists.");
-                }
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
+        }
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
         }
     }
 }
